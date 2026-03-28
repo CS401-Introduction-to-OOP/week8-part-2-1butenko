47d0f0a0f7588f5aa9a765197ce97865734e3b3c
1butenko <ihor.butenko@icloud.com>
2026-03-28 15:45:24 +0200

    File resource

---
+++ Week8/Variant1/FileResource.cs
@@ -0,0 +1,20 @@
+namespace Variant1;
+public class FileResource : Resource, IDisposable
+{
+public FileResource(string name) : base(name){}
+public override void Open()
+{
+    IsOpen = true;
+    Console.WriteLine($"File opened: {Name}");
+}
+public override void Close()
+{
+    IsOpen = false;
+    Console.WriteLine($"Closed: {Name}");
+}
+public void Dispose()
+{
+    Close();
+    Console.WriteLine($"[FileResource] Disposed: {Name}");
+}
+}
