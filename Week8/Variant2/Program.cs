46bd11ec6ae55b3de3f604445f88655aca340f50
1butenko <ihor.butenko@icloud.com>
2026-03-28 16:42:30 +0200

    Fixes for Variant 3

--- Week8/Variant2/Program.cs
+++ Week8/Variant2/Program.cs
@@ -1,17 +1,23 @@
-using Variant2;
+namespace Variant2;
 
-Point p1 = new Point { X = 1, Y = 2 };
-Point p2 = p1;
-p2.X = 99;                          
-Console.WriteLine($"p1 = {p1}");
-Console.WriteLine($"p2 = {p2}");
-Console.WriteLine();
+public class ProgramVariant2
+{
+    public static void Run()
+    {
+        Point p1 = new Point { X = 1, Y = 2 };
+        Point p2 = p1;
+        p2.X = 99;                          
+        Console.WriteLine($"p1 = {p1}");
+        Console.WriteLine($"p2 = {p2}");
+        Console.WriteLine();
 
-PointRef r1 = new PointRef { X = 1, Y = 2 };
-PointRef r2 = r1;
-r2.X = 99;
-Console.WriteLine($"r1 = {r1}");
-Console.WriteLine($"r2 = {r2}");
-Console.WriteLine();
+        PointRef r1 = new PointRef { X = 1, Y = 2 };
+        PointRef r2 = r1;
+        r2.X = 99;
+        Console.WriteLine($"r1 = {r1}");
+        Console.WriteLine($"r2 = {r2}");
+        Console.WriteLine();
 
-BoxingTester.Test();
+        BoxingTester.Test();
+    }
+}
