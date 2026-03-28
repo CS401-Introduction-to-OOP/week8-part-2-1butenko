9fa7c709c9e48c565adb806b955aaa9718b6facb
1butenko <ihor.butenko@icloud.com>
2026-03-28 16:06:44 +0200

    Fixed Boxing tester

--- Week8/Variant2/BoxingTester.cs
+++ Week8/Variant2/BoxingTester.cs
@@ -4,13 +4,13 @@ public static class BoxingTester
 {
     public static void Test()
     {
-        int a = 10;
+        int a = 10; // create int a
         Console.WriteLine($"{a} | type={a.GetType()}");
 
-        object b = a;
+        object b = a; // change it to reference -> boxing
         Console.WriteLine($"{b} | type={b.GetType()}");
 
-        int c = (int)b;
+        int c = (int)b; // change it back to int -> unboxing
         Console.WriteLine($"{c} | type={c.GetType()}");
     }
 }
